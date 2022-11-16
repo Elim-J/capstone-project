80983198e295a90fbeb445fbb732f27c05ac3735
ErikWenslaff <epw@uwm.edu>
2022-11-15 19:53:34 -0600

    Added a picklist for AStar to choose which heuristic to use

--- src/components/visualizers/shared/CodeContent.js
+++ src/components/visualizers/shared/CodeContent.js
@@ -73,6 +73,24 @@ const CodeContent = ({alg, open, setOpen, getMessage}) => {
         </div>
     );
 
+    const AStar = (
+        <div className="code-content">
+            <pre id="code-0">{'passes = 0'}</pre>
+            <pre id="code-1">{'hasSwapped = false'}</pre>
+            <pre id="code-2">{'do {'}</pre>
+            <pre id="code-3">{'   ++passes;'}</pre>
+            <pre id="code-4">{'   for (i = 0; i < arr.length - passes; i++){'}</pre>
+            <pre id="code-5">{'     if (arr[i] > arr[i+1]){'}</pre>
+            <pre id="code-6">{'          const swap = arr[i]'}</pre>
+            <pre id="code-7">{'          arr[i] = arr[i+1]'}</pre>
+            <pre id="code-8">{'          arr[i+1] = swap'}</pre>
+            <pre id="code-9">{'          hasSwapped = true'}</pre>
+            <pre id="code-10">{'     }'}</pre>
+            <pre id="code-11">{'   }'}</pre>
+            <pre id="code-12">{'} while(hasSwapped)'}</pre>
+        </div>
+    );
+
     // console.log(getMessage());
     return (
         <Collapse in={open}>
