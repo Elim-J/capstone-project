9e19c5d9dfc6ed18c7a43e6fe6535d7c403597dc
ErikWenslaff <epw@uwm.edu>
2022-11-17 18:42:12 -0600

    small fix for Selection Sort CodeContent

--- src/components/visualizers/shared/CodeContent.js
+++ src/components/visualizers/shared/CodeContent.js
@@ -83,13 +83,13 @@ const CodeContent = ({alg, open, setOpen, getMessage}) => {
             <pre id="code-5">{'          if (ints[j] < ints[minIndex]) {'}</pre>
             <pre id="code-6">{'              min_idx = j;'}</pre>
             <pre id="code-7">{'          }'}</pre>
-            <pre id="code-9">{'      }'}</pre>
+            <pre id="code-8">{'      }'}</pre>
             <pre id="code-9">{'      let temp = ints[minIndex];'}</pre>
-            <pre id="code-9">{'      ints[minIndex] = ints[i];'}</pre>
-            <pre id="code-9">{'      ints[i] = temp;'}</pre>
-            <pre id="code-10">{'   }'}</pre>
-            <pre id="code-11">{'   return ints'}</pre>
-            <pre id="code-12">{'}'}</pre>
+            <pre id="code-10">{'      ints[minIndex] = ints[i];'}</pre>
+            <pre id="code-11">{'      ints[i] = temp;'}</pre>
+            <pre id="code-12">{'   }'}</pre>
+            <pre id="code-13">{'   return ints'}</pre>
+            <pre id="code-14">{'}'}</pre>
         </div>
     );
 
@@ -221,6 +221,48 @@ const CodeContent = ({alg, open, setOpen, getMessage}) => {
             <pre id="code-18">{'}'}</pre>
         </div>
     )
+
+    const BFS = (
+        <div className="code-content">
+            <pre id="code-1">{'function AStarManhattan(startNode, endNodes){'}</pre>
+            <pre id="code-2">{'   startNode.heuristic = calcHeuristic(node, endNodes);'}</pre>
+            <pre id="code-3">{'   startNode.prev = NULL'}</pre>
+            <pre id="code-4">{'   priorityQueue.offer(startNode)'}</pre>
+            <pre id="code-5">{'   hasSwapped = false'}</pre>
+            <pre id="code-6">{'   while(!priorityQueue.isEmpty()){'}</pre>
+            <pre id="code-7">{'      let node = priorityQueue.poll();'}</pre>
+            <pre id="code-8">{'      if (node.isEnd) {'}</pre>
+            <pre id="code-9">{'         let path = [];'}</pre>
+            <pre id="code-10">{'         while (node){'}</pre>
+            <pre id="code-11">{'            path.insert(node);'}</pre>
+            <pre id="code-12">{'            node = node.prev;'}</pre>
+            <pre id="code-13">{'         }'}</pre>
+            <pre id="code-14">{'      }'}</pre>
+            <pre id="code-15">{'      node.above.heuristic = calcHeuristic(node.above, endNodes);'}</pre>
+            <pre id="code-16">{'      node.above.prev = node;'}</pre>
+            <pre id="code-17">{'      priorityQueue.offer(node.above'}</pre>
+            <pre id="code-18">{'      node.right.heuristic = calcHeuristic(node.right, endNodes);'}</pre>
+            <pre id="code-19">{'      node.right.prev = node;'}</pre>
+            <pre id="code-20">{'      priorityQueue.offer(node.right'}</pre>
+            <pre id="code-21">{'      node.below.heuristic = calcHeuristic(node.below, endNodes);'}</pre>
+            <pre id="code-22">{'      node.below.prev = node;'}</pre>
+            <pre id="code-23">{'      priorityQueue.offer(node.below'}</pre>
+            <pre id="code-24">{'      node.left.heuristic = calcHeuristic(node.left, endNodes);'}</pre>
+            <pre id="code-25">{'      node.left.prev = node;'}</pre>
+            <pre id="code-26">{'      priorityQueue.offer(node.left'}</pre>
+            <pre id="code-27">{'   }'}</pre>
+            <pre id="code-28">{'}'}</pre>
+            <pre id="code-29">{''}</pre>
+            <pre id="code-30">{'function calcHeuristic(node, endNodes[]){'}</pre>
+            <pre id="code-31">{'   let minHeuristic;'}</pre>
+            <pre id="code-32">{'   endNodes.forEach(endNode => {'}</pre>
+            <pre id="code-33">{'      let distance = Math.abs(node.x - endNode.x) + Math.abs(node.y - endNode.y);'}</pre>
+            <pre id="code-34">{'      minHeuristic = Math.min(minHeuristic, distance);'}</pre>
+            <pre id="code-35">{'   }'}</pre>
+            <pre id="code-36">{'   return minHeuristic;'}</pre>
+            <pre id="code-37">{'}'}</pre>
+        </div>
+    );
 
     const AStarManhattan = (
         <div className="code-content">
