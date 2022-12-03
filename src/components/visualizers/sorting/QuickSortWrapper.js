c53d6812ac202d2b600b38db12d7bf9e7543d975
ErikWenslaff <epw@uwm.edu>
2022-12-02 21:25:27 -0600

    made updates to work with main

--- src/components/visualizers/sorting/QuickSortWrapper.js
+++ src/components/visualizers/sorting/QuickSortWrapper.js
@@ -8,6 +8,7 @@ import CodeContent from '../shared/CodeContent';
 import '../../../css/quicksort.css';
 import QuickSortActionBar from './QuickSortActionBar.js';
 
+//style={containerStyles}
 export default function QuickSortWrapper() {
 
   let tempInts = Array.from({length: 10}, () => Math.floor(Math.random() * 100));
@@ -15,15 +16,13 @@ export default function QuickSortWrapper() {
   const [vid, setVid] = useState([]);
   const [currentFrame, setCurrentFrame] = useState(0);
   const [isPaused, setIsPaused] = useState(true);
-  // const [currentSpeed, setCurrentSpeed] = useState(500);
+  const [speed, setSpeed] = useState(500);
   const [timeoutId, setTimeoutId] = useState(null);
-  const [pivot, setPivot] = useState('random');
+  const [pivot, setPivot] = useState(QuickSortPivots.Random);
   const [ints, setInts] = useState(tempInts);
-  const [speed, setSpeed] = useState(DefaultSpeed);
   const [openCode, setOpenCode] = useState(true);
+  const [alg, setAlg] = useState(SortingAlgs.QuickSort)
   let frame = useRef(currentFrame);
-
-  const currentSpeed = useRef(speed);
 
   useEffect(() => {
     // handleRandomArr();
@@ -35,13 +34,16 @@ export default function QuickSortWrapper() {
     highlightCodeLine(vid[currentFrame]?.highlightedLines);
   }, [currentFrame]);
 
-  const getMessage = () => {
-    if (vid && vid[currentFrame] && vid[currentFrame].message){
-        return vid[currentFrame].message;
-    } else {
-        return "";
-    }
-  };
+  const renderRectSvgNode = ({ nodeDatum, toggleNode }) => (
+    <g className="r3dt-node">
+      
+      {/* <rect r="15" x="0" y="10" width="130" height="100" fill="none"></rect> */}
+      <g className="r3dt-label">
+          <text className='Numbers' style={{fontSize:'35px', fontStyle:'normal'}}>{nodeDatum.data}</text>
+      </g>
+      
+    </g>
+  );
 
   const highlightCodeLine = (lines) => {
     console.log('highlighting ' + lines);
@@ -54,114 +56,13 @@ export default function QuickSortWrapper() {
     });
   };
 
-  const handleRandomArr = () => {
-    setCurrentFrame(0);
-    setInts(Array.from({length: 10}, () => Math.floor(Math.random() * 100)));
-    frame.current = 0;
-    setVid(quickSort(ints, QuickSortPivots.Random));
-  };
-
-  const handleStepForward = () => {
-    if(vid && frame.current < vid.length - 1){
-      console.log("stepping forward " + frame.current);
-      setCurrentFrame(frame.current + 1);
-      frame.current++;
-    }
-  };
-  
-  const handleStepBackward = () => {
-    if(vid && frame.current > 0){
-      console.log("stepping back");
-      setCurrentFrame(frame.current - 1);
-      frame.current--;
-    }
-  };
-
-  function handleChangeSpeed(e){
-    if (e == Speed.FourX){
-        setSpeed(speed => {
-                        currentSpeed.current = DefaultSpeed / 4;
-                        return DefaultSpeed / 4;
-                    });
-    }
-    else if (e == Speed.TwoX){
-        setSpeed(speed => {
-            currentSpeed.current = DefaultSpeed / 2;
-            return DefaultSpeed / 2;
-        });
-    }
-    else if (e == Speed.OneX){
-        setSpeed(speed => {
-            currentSpeed.current = DefaultSpeed;
-            return DefaultSpeed;
-        });
-    }
-    else if (e == Speed.HalfX){
-        setSpeed(speed => {
-            currentSpeed.current = DefaultSpeed * 2;
-            return DefaultSpeed * 2;
-        });
-    }
-    else if (e == Speed.QuarterX){
-        setSpeed(speed => {
-            currentSpeed.current = DefaultSpeed * 4;
-            return DefaultSpeed * 4;
-        });
-    }
-}
-
-  const handlePlayAndPause = () => {
-        
-    if (isPaused) {
-        setIsPaused(false);
-        console.log("In Play/Pause rnnnnnn")
-
-        const stepForward = () => {
-          if(frame.current < vid.length - 2){
-            handleStepForward();
-            console.log("in play" );
-            const timeoutId = setTimeout(stepForward, currentSpeed);
-            setTimeoutId(timeoutId);
-          }
-          else
-          {
-            handlePause();
-          }
-        }
-
-        const timeoutId = setTimeout(stepForward, currentSpeed);
-        setTimeoutId(timeoutId);
+  const getMessage = () => {
+    if (vid && vid[currentFrame] && vid[currentFrame].message){
+        return vid[currentFrame].message;
     } else {
-        handlePause();
-        // setTimeoutId(null);
+        return "";
     }
-};
-
-const handlePause = () => {
-  if(!isPaused){
-      setIsPaused(true);
-      clearTimeout(timeoutId);
-      setTimeoutId(null);
-  }
-};
-
-const handlePivot = (e) => {
-  setPivot(e);
-  setCurrentFrame(0);
-  frame.current = 0;
-  setVid(quickSort(ints, e));
-}
-
-  const renderRectSvgNode = ({ nodeDatum, toggleNode }) => (
-    <g className="r3dt-node">
-      
-      {/* <rect r="15" x="0" y="10" width="130" height="100" fill="none"></rect> */}
-      <g className="r3dt-label">
-          <text className='Numbers' style={{fontSize:'35px', fontStyle:'normal'}}>{nodeDatum.data}</text>
-      </g>
-      
-    </g>
-  );
+  };
 
   const useCenteredTree = (defaultTranslate = { x: 0, y: 0 }) => {
     const [translate, setTranslate] = useState(defaultTranslate);
@@ -180,24 +81,27 @@ const handlePivot = (e) => {
   const [dimensions, translate, containerRef] = useCenteredTree();
   const separation = { nonSiblings: 4, siblings: 4 }
   const scaleExtent= { min: 0.1, max: 3 }
-  
   return (
     <>
     <div className='body'>
 
-        {vid[frame.current]?.rootTree &&
-        <Tree
-          data={vid[frame.current].rootTree}
-          dimensions={dimensions}
-          translate={translate}
-          renderCustomNodeElement={renderRectSvgNode}
-          orientation="vertical"
-          pathFunc="straight"
-          depthFactor="150"
-          separation = {separation}
-          scaleExtent = {scaleExtent}
-        />}
-                  <div className="code-wrapper">
+    
+    {console.log('rerender')}
+    <div className="tree-container" ref={containerRef}>
+
+      {vid[currentFrame]?.rootTree &&
+      <Tree
+      data={vid[currentFrame].rootTree}
+      dimensions={dimensions}
+      translate={translate}
+      renderCustomNodeElement={renderRectSvgNode}
+      orientation="vertical"
+      pathFunc="straight"
+      depthFactor="150"
+      separation = {separation}
+      scaleExtent = {scaleExtent}
+    />}
+    <div className="code-wrapper">
                     <CodeContent 
                     alg={SortingAlgs.QuickSort} 
                     open={openCode} 
@@ -206,48 +110,28 @@ const handlePivot = (e) => {
                     pivot={pivot}
                     />
                 </div>
-        
-      </div>
-          <Button onClick={handleStepBackward}>Step back</Button>
-          <Button onClick={handlePlayAndPause}>Play/Pause</Button>
-          <Button onClick={handleStepForward}>Step forward</Button>
-          <Button onClick={handleRandomArr}>Generate Random Data</Button>
-          <label className="dropdown">
-                        <select className="form-select algorithm-dropdown-toggle" id="alg-select"
-                            onChange={(e) => {
-                                handlePivot(e.target.value);
-                            }}
-                            defaultValue={QuickSortPivots.Random}>
-                                {Object.values(QuickSortPivots).map(val => (
-                                    <option
-                                        aria-selected="true"
-                                        key={val}
-                                        value={val}
-                                        >
-                                        {val}
-                                    </option>
-                                    )
-                                )}
-                        </select>
-                    </label>
-            <label className="dropdown">
-                        <select className="form-select algorithm-dropdown-toggle" id="alg-select"
-                            onChange={(e) => {
-                                handleChangeSpeed(e.target.value);
-                            }}
-                            defaultValue={Speed.OneX}>
-                                {Object.values(Speed).map(val => (
-                                    <option
-                                        aria-selected="true"
-                                        key={val}
-                                        value={val}
-                                        >
-                                        {val}
-                                    </option>
-                                    )
-                                )}
-                        </select>
-                    </label>   
-      </>
-    );
-  }
+      
+    </div>
+    
+    <QuickSortActionBar
+      currentFrame={currentFrame}
+      setCurrentFrame={setCurrentFrame}
+      vid={vid}
+      setVid={setVid}
+      speed={speed}
+      setSpeed={setSpeed}
+      isPaused={isPaused}
+      setIsPaused={setIsPaused}
+      pivot={pivot}
+      setPivot={setPivot}
+      ints={ints}
+      setAlg={setAlg}
+      // openCode={openCode}
+      // setOpenCode={setOpenCode}
+      // openInfo={openInfo}
+      // setOpenInfo={setOpenInfo}
+      />
+    </div>  
+    </>
+  );
+}
